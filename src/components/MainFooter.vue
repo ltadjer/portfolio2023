ae190ad891c0d6b19e4c43812d74c08cd1a4d993
ltadjer <94218573+ltadjer@users.noreply.github.com>
2023-04-15 18:11:41 +0200

    update rs footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -2,17 +2,17 @@
   <div class="main-footer">
     <div class="container-rs">
       <div class="icon-container">
-        <a href="https://www.linkedin.com/in/liticia-tadjer"
+        <a href="https://www.linkedin.com/in/liticia-tadjer" target="_blank"
           ><img src="@/assets/icons/linkedin.svg" alt="Lien linkedin"
         /></a>
       </div>
       <div class="icon-container">
-        <a href="https://github.com/ltadjer"
+        <a href="https://github.com/ltadjer/portfolio2023" target="_blank"
           ><img src="@/assets/icons/github.svg" alt="Lien github"
         /></a>
       </div>
       <div class="icon-container">
-        <a href="mailto:tadjerliticia@gmail.com"
+        <a href="mailto:tadjerliticia@gmail.com" target="_blank"
           ><img src="@/assets/icons/mail.svg" alt="Lien e-mail"
         /></a>
       </div>
